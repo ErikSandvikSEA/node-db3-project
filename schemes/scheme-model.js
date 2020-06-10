e731481a57aa1adf651cd94f4110a10d08ffa402
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-10 14:45:54 -0700

    added findSteps()

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,27 @@
 const db = require('../data/dbconfig')
 
 function find(){
-     return db('schemes as s')
+     return db('schemes')
 }
+
+function findById(id){
+     return db('schemes')
+          .where('id', id)
+          .first()
+}
+
+function findSteps(id){
+     return db('steps')
+          .join('schemes', 'steps.scheme_id', 'schemes.id')
+          .where('schemes.id', id)
+          .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+          .orderBy('steps.step_number')
+}
+
+
 
 module.exports = {
      find,
+     findById,
+     findSteps
 }
